287efcba957b6145ced78524183996ea07f5265e
Baptiste Fontaine <batifon@yahoo.fr>
2013-01-11 01:42:49 +0100

    Path issue fixed.

--- tests/tests.rb
+++ tests/tests.rb
@@ -4,7 +4,7 @@
 require 'test/unit'
 require_relative '../lib/graph'
 
-for t in Dir.glob(File.dirname(__FILE__)+'/*_tests.rb')
+for t in Dir.glob( File.join( File.expand_path( File.dirname(__FILE__) ),  '*_tests.rb' ) )
     require t
 end
 
