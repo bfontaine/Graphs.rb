07ee4da6030774eb69fd23599a55590f1379045f
Baptiste Fontaine <batifon@yahoo.fr>
2013-01-15 01:13:08 +0100

    more JSON tests

--- tests/json_tests.rb
+++ tests/json_tests.rb
@@ -60,3 +60,74 @@ class JSON_Graph_test < Test::Unit::TestCase
     end
 
 end
+
+class JSON_test < Test::Unit::TestCase
+
+    # == JSON::parse == #
+
+    def test_parse_empty_graph
+        g = JSONGraph::parse('')
+
+        assert_equal([], g.nodes)
+        assert_equal([], g.edges)
+    end
+
+    def test_parse_empty_graph_with_nodes_list
+
+        s = '{"nodes":[]}' 
+
+        g = JSONGraph::parse(s)
+
+        assert_equal([], g.nodes)
+        assert_equal([], g.edges)
+    end
+
+    def test_parse_empty_graph_with_nodes_and_edges_lists
+        s = '{"nodes":[],"edges":[]}'
+        g = JSONGraph::parse(s)
+
+        assert_equal([], g.nodes)
+        assert_equal([], g.edges)
+    end
+
+    def test_parse_one_node_no_edge
+        s = '{"nodes":[{"label":"foo"}]}'
+        g = JSONGraph::parse(s)
+
+        assert_equal(1, g.nodes.length)
+        assert_equal('foo', g.nodes[0]['label'])
+        assert_equal([], g.edges)
+    end
+
+    def test_parse_sample_graph
+        g = JSONGraph::parse(JSONUtils::get_sample_graph)
+
+        assert_equal(2, g.nodes.length)
+        assert_equal(1, g.edges.length)
+
+        assert_equal('foo', g.nodes[0]['label'])
+        assert_equal('bar', g.nodes[1]['label'])
+
+        assert_equal('bar', g.edges[0]['node1'])
+        assert_equal('foo', g.edges[0]['node2'])
+
+    end
+
+    # == JSON::unparse == #
+
+    def test_unparse_empty_graph
+        g = Graph.new
+
+        s = JSONGraph::unparse(g)
+
+        assert_equal('{"nodes":[],"edges":[]}', s)
+    end
+
+    def test_unparse_sample_graph
+        g1 = JSONGraph::parse(JSONUtils::get_sample_graph)
+        g2 = JSONGraph::parse(JSONGraph::unparse(g1))
+
+        assert_equal(g1, g2)
+    end
+
+end
