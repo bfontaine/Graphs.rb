8e05758f86be29ab9b1b71e1cd357c6eeed420b8
Baptiste Fontaine <batifon@yahoo.fr>
2013-02-13 15:03:06 +0100

    Using simplecov for code coverage.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
 
 gem 'rake', :group => :test
+gem 'simplecov', :require => false, :group => :test
--- tests/tests.rb
+++ tests/tests.rb
@@ -3,9 +3,14 @@
 
 require 'test/unit'
 require 'tempfile'
+require 'simplecov'
+
+SimpleCov.start { add_filter '/tests/' } if ENV['COVERAGE']
+
 require_relative '../lib/graph'
+require_relative '../lib/graphs/gdf'
+require_relative '../lib/graphs/json'
 
 for t in Dir.glob( File.join( File.expand_path( File.dirname(__FILE__) ),  '*_tests.rb' ) )
     require t
 end
-
