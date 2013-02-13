bd8a2c854d9751e082b8de9dc89cdd3ee104e93f
Baptiste Fontaine <batifon@yahoo.fr>
2013-02-13 15:40:40 +0100

    Fixed code coverage.

--- tests/tests.rb
+++ tests/tests.rb
@@ -5,12 +5,16 @@ require 'test/unit'
 require 'tempfile'
 require 'simplecov'
 
+test_dir = File.expand_path( File.dirname(__FILE__) )
+
 SimpleCov.start { add_filter '/tests/' } if ENV['COVERAGE']
 
 require_relative '../lib/graph'
 require_relative '../lib/graphs/gdf'
 require_relative '../lib/graphs/json'
 
-for t in Dir.glob( File.join( File.expand_path( File.dirname(__FILE__) ),  '*_tests.rb' ) )
+for t in Dir.glob( File.join( test_dir,  '*_tests.rb' ) )
     require t
 end
+
+exit Test::Unit::AutoRunner.run
